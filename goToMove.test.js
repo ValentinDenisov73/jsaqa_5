d55dace9d0520b44ee86b3b0024fce9752b0ad1e
Valentin Denisov <valentindenisov1989@gmail.com>
2023-05-30 15:50:57 +0400

    fix code

--- goToMove.test.js
+++ goToMove.test.js
@@ -18,6 +18,7 @@ describe("GoToMoveTests", () => {
     test("ChosenOneSeat", async ()=>{
         await clickElement (page, "div:nth-child(3) > span:nth-child(5)");
         await clickElement (page, "button");
+        await page.waitForSelector("h2");
         const actual = await page.$eval("h2", (link) => link.textContent);
         const expected = "Вы выбрали билеты:";
         expect(actual).toContain(expected);
@@ -27,6 +28,7 @@ describe("GoToMoveTests", () => {
         await clickElement (page, "div:nth-child(3) > span:nth-child(5)");
         await clickElement (page, "div:nth-child(3) > span:nth-child(6)");
         await clickElement (page, "button");
+        await page.waitForSelector("h2");
         const actual = await page.$eval("h2", (link) => link.textContent);
         const expected = "Вы выбрали билеты:";
         expect(actual).toContain(expected);
